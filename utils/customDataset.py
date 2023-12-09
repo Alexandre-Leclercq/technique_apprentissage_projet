53688eaf59cd86d3389b5ee3c0c842bf490ae752
Alexandre-Leclercq <69433685+Alexandre-Leclercq@users.noreply.github.com>
2023-12-09 13:03:49 -0500

    update tensor implementation for the custom dataset

--- utils/customDataset.py
+++ utils/customDataset.py
@@ -9,14 +9,20 @@ from typing import Tuple
 
 class CustomDataset(Dataset):
 
-    def __init__(self, targ_dir: str, filenames: np.array, targets, classes):
+    DATA_TYPE = torch.float32
+
+    def __init__(self, targ_dir: str, filenames: np.array, targets: np.array, classes, img_size: int):
+        self._device = torch.device('cpu')
+        if torch.backends.cuda.is_built():
+            self._device = torch.device('cuda')
+
         self.paths = [pathlib.Path(targ_dir) / (str(filename) + '.jpg') for filename in filenames]
         self.classes = classes
-        self.targets = targets
+        self.targets = torch.tensor(targets, dtype=self.DATA_TYPE, device=self._device)
         self.transform = transforms.Compose([
-            transforms.Resize((128, 128)),
+            transforms.Resize((img_size, img_size)),
             transforms.ToTensor(),
-            transforms.Normalize(0.5, 0.5)  # Normalize for [0,1] to [-1, 1]
+            transforms.Normalize(0.5, 0.5),  # Normalize for [0,1] to [-1, 1]
         ])
 
     def load_image(self, index: int) -> Image.Image:
@@ -28,4 +34,4 @@ class CustomDataset(Dataset):
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
         img: Image.Image = self.load_image(index)
 
-        return self.transform(img), self.targets[index]
+        return self.transform(img).to(dtype=self.DATA_TYPE, device=self._device), self.targets[index]
